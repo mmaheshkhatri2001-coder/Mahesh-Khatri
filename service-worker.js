53b2c1c13006a13da1079ed0e2d7f51899a6d0b2
mmaheshkhatri2001-coder <mmaheshkhatri2001@gmail.com>
2026-05-03 12:58:07 +0545

    Create service-worker.js with offline caching functionality for PWA support

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Cache name
+const CACHE_NAME = 'my-site-cache-v1';
+
+// Files to cache
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js'
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch and cache logic
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return the response from the cached version
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate the service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
